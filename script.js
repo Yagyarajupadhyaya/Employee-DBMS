a58fd49566826b25989ce05e45c85093b29c611b
Yagyarajupadhyaya <yagya415@gmail.com>
2025-12-01 13:40:59 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-(async function (){
+(async function () {
 
     const data = await fetch("data.json");
     const res = await data.json();
     let employees = res;
 
-    let selectedEmployeeId = employees[0].id;
+    let selectedEmployeeId = employees[0]?.id;
     let selectedEmployee = employees[0];
 
     const employeeList = document.querySelector(".employees__names--list");
@@ -14,23 +14,23 @@
     const addEmployeeModal = document.querySelector(".addEmployee");
     const addEmployeeForm = document.querySelector(".addEmployee_create");
 
-
-    //show add employee modal
+    // show add employee modal
     createEmployee.addEventListener("click", () => {
-        addEmployeeModal.computedStyleMap.display = "flex";
+        addEmployeeModal.style.display = "flex";
     });
 
     // Close modal when clicking outside form
-
     addEmployeeModal.addEventListener("click", (e) => {
-        if(e.target.className === "addEmployee"){
-            addEmployeeModal.style.display = "none"
+        if (e.target.className === "addEmployee") {
+            addEmployeeModal.style.display = "none";
         }
     });
 
-    // Restrict DOB  input -> Minimum 18 years old
+    // Restrict DOB input -> Minimum 18 years old
     const dobInput = document.querySelector(".addEmployee_create--dob");
-    dobInput.max = `${new Date().getFullYear() - 18}- ${new Date().toISOString().slice(5, 10)}`;
+    const today = new Date();
+    const year = today.getFullYear() - 18;
+    const monthDay = today.toISOString().slice(5, 10); // MM-DD
+    dobInput.max = `${year}-${monthDay}`;
 
-    
-});
+})();
